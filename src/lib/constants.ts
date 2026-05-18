4b4119ddad3b5363e9bd42ff5df583ee7b754083
giangvo7979 <v.tragiang79@gmail.com>
2026-05-18 17:37:42 +0700

    Update - Bum

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -24,4 +24,4 @@ export const WALRUS_EPOCHS_RESPONSE = 10;
 export const WALRUS_EPOCHS_FILE = 10;
 
 export const NETWORK = import.meta.env.VITE_NETWORK ?? 'mainnet';
-export const SEAL_THRESHOLD = 1;
+export const SEAL_THRESHOLD = 2;
